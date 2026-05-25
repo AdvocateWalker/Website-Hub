e64273caebcaa6460c992ce9fdc21a53cad6999f
AdvocateWalker <themasterzplayz@gmail.com>
2026-05-24 20:00:39 -0400

    adv changes

---
+++ js/adv-site.js
@@ -0,0 +1,5 @@
+﻿(function () {
+  const yearEls = document.querySelectorAll("[data-year]");
+  const year = new Date().getFullYear();
+  yearEls.forEach((el) => (el.textContent = String(year)));
+})();
